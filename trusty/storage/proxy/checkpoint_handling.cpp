8a49a860d01e5391fe72cbdceaf41c89ad8e3871
Weston Carvalho <westoncarvalho@google.com>
2025-03-04 13:48:02 -0600

    storageproxyd: Fail if unable to connect to vold
    Make storageproxyd fail if it's unable to register a checkpoint listener
    to vold. This also means:
    
      * is_data_checkpointing cannot fail
      * proxy will never respond to STORAGE_CHECKPOINTING_STATE requests
        with STORAGE_CHECKPOINT_STATE_UNKNOWN
    
    Bug: 393406566
    Test: wip
    Change-Id: Ia7f246af21eb36c8a33ece8d2eae044b442f14e1

--- trusty/storage/proxy/checkpoint_handling.cpp
+++ trusty/storage/proxy/checkpoint_handling.cpp
@@ -18,7 +18,6 @@
 #include "ipc.h"
 #include "log.h"
 
-#include <fstab/fstab.h>
 #include <unistd.h>
 #include <cstring>
 #include <string>
@@ -41,9 +40,6 @@ using ::ndk::SharedRefBase;
 
 const char kVoldService[] = "android.system.vold.IVold/default";
 
-/* Written once from main thread before any checkpointing_get_state
- * calls could be reading it. */
-bool voldConnected = false;
 std::atomic<bool> voldPossibleCheckpointing = true;
 
 class VoldListener : public BnVoldCheckpointListener {
@@ -57,59 +53,8 @@ class VoldListener : public BnVoldCheckpointListener {
 
 }  // namespace
 
-int is_data_checkpoint_active(bool* active) {
-    if (!active) {
-        ALOGE("active out parameter is null");
-        return 0;
-    }
-
-    if (voldConnected) {
-        *active = voldPossibleCheckpointing.load();
-        return 0;
-    }
-
-    *active = false;
-
-    static bool checkpointingDoneForever = false;
-    if (checkpointingDoneForever) {
-        return 0;
-    }
-
-    android::fs_mgr::Fstab procMounts;
-    bool success = android::fs_mgr::ReadFstabFromFile("/proc/mounts", &procMounts);
-    if (!success) {
-        ALOGE("Could not parse /proc/mounts\n");
-        /* Really bad. Tell the caller to abort the write. */
-        return -1;
-    }
-
-    android::fs_mgr::FstabEntry* dataEntry =
-            android::fs_mgr::GetEntryForMountPoint(&procMounts, "/data");
-    if (dataEntry == NULL) {
-        ALOGE("/data is not mounted yet\n");
-        return 0;
-    }
-
-    /* We can't handle e.g., ext4. Nothing we can do about it for now. */
-    if (dataEntry->fs_type != "f2fs") {
-        ALOGW("Checkpoint status not supported for filesystem %s\n", dataEntry->fs_type.c_str());
-        checkpointingDoneForever = true;
-        return 0;
-    }
-
-    /*
-     * The data entry looks like "... blah,checkpoint=disable:0,blah ...".
-     * checkpoint=disable means checkpointing is on (yes, arguably reversed).
-     */
-    size_t checkpointPos = dataEntry->fs_options.find("checkpoint=disable");
-    if (checkpointPos == std::string::npos) {
-        /* Assumption is that once checkpointing turns off, it stays off */
-        checkpointingDoneForever = true;
-    } else {
-        *active = true;
-    }
-
-    return 0;
+bool is_data_checkpoint_active() {
+    return voldPossibleCheckpointing.load();
 }
 
 /**
@@ -129,13 +74,13 @@ int vold_connect() {
     if (binder == nullptr) {
         ALOGD("Got null binder for %s (was %sdeclared)\n", kVoldService,
               AServiceManager_isDeclared(kVoldService) ? "" : "not ");
-        return 0;
+        return -1;
     }
 
     auto vold = IVold::fromBinder(binder);
     if (vold == nullptr) {
         ALOGI("Could not convert binder to android::system::vold::IVold\n");
-        return 0;
+        return -1;
     }
 
     CheckpointingState state;
@@ -143,7 +88,7 @@ int vold_connect() {
             vold->registerCheckpointListener(SharedRefBase::make<VoldListener>(), &state);
     if (!ret.isOk()) {
         ALOGE("Could not register VoldCheckpointListener: %s\n", ret.getDescription().c_str());
-        return 0;
+        return -1;
     }
     ALOGI("Registered VoldCheckpointListener in %s\n", toString(state).c_str());
 
@@ -151,14 +96,10 @@ int vold_connect() {
         voldPossibleCheckpointing.store(false);
     }
 
-    voldConnected = true;
     return 0;
 }
 
 static storage_checkpoint_state checkpointing_state() {
-    if (!voldConnected) {
-        return STORAGE_CHECKPOINT_STATE_UNKNOWN;
-    }
     if (voldPossibleCheckpointing.load()) {
         return STORAGE_CHECKPOINT_STATE_POSSIBLE;
     }
