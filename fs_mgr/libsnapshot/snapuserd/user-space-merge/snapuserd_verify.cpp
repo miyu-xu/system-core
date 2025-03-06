915a4ad4b89352ac5b0e640bc508b913f68f9ed8
Akilesh Kailash <akailash@google.com>
2025-03-06 15:17:22 -0800

    Verify partitions using registered buffers
    1: Single thread per partition is used for verification but with a
       default queue depth of 4
    2: Since the buffers are pre-registered, buffer allocation will
       not be from CMA memory.
    
    Partition verification when every block is a COPY operation:
    
    Partition: system_dlkm Duration : 123 ms
    Partition: vendor_dlkm Duration : 135 ms
    Partition: system_ext Duration : 1421 ms
    Partition: system Size: 945278976 Duration : 2834 ms
    Partition: vendor Size: 998957056 Duration : 2881 ms
    Partition: product Size: 3941609472 Duration : 6246 ms
    
    Bug: 400674947
    Test: Boot device off snapshots and verify partition verification is
    successful. OTA on Pixel
    Change-Id: Ife88c13e7ef00a46aef55e755ae841e33bc7e913
    Signed-off-by: Akilesh Kailash <akailash@google.com>

--- fs_mgr/libsnapshot/snapuserd/user-space-merge/snapuserd_verify.cpp
+++ fs_mgr/libsnapshot/snapuserd/user-space-merge/snapuserd_verify.cpp
@@ -22,6 +22,7 @@
 
 #include "android-base/properties.h"
 #include "snapuserd_core.h"
+#include "utility.h"
 
 namespace android {
 namespace snapshot {
@@ -96,50 +97,104 @@ void UpdateVerify::VerifyUpdatePartition() {
 }
 
 bool UpdateVerify::VerifyBlocks(const std::string& partition_name,
-                                const std::string& dm_block_device, off_t offset, int skip_blocks,
-                                uint64_t dev_sz) {
+                                const std::string& dm_block_device, uint64_t dev_sz) {
     unique_fd fd(TEMP_FAILURE_RETRY(open(dm_block_device.c_str(), O_RDONLY | O_DIRECT)));
     if (fd < 0) {
         SNAP_LOG(ERROR) << "open failed: " << dm_block_device;
         return false;
     }
 
-    loff_t file_offset = offset;
-    auto verify_block_size = android::base::GetUintProperty<uint>("ro.virtual_ab.verify_block_size",
-                                                                  kBlockSizeVerify);
-    const uint64_t read_sz = verify_block_size;
+    int queue_depth = std::max(queue_depth_, 1);
+    int verify_block_size = verify_block_size_;
 
-    void* addr;
-    ssize_t page_size = getpagesize();
-    if (posix_memalign(&addr, page_size, read_sz) < 0) {
-        SNAP_PLOG(ERROR) << "posix_memalign failed "
-                         << " page_size: " << page_size << " read_sz: " << read_sz;
+    // Smaller partitions don't need a bigger queue-depth.
+    // This is required for low-memory devices.
+    if (dev_sz < threshold_size_) {
+        queue_depth = std::max(queue_depth / 2, 1);
+        verify_block_size >>= 2;
+    }
+
+    if (!IsBlockAligned(verify_block_size)) {
+        verify_block_size = EXT4_ALIGN(verify_block_size, BLOCK_SZ);
+    }
+
+    SNAP_LOG(DEBUG) << "VerifyBlockS: queue_depth: " << queue_depth
+                    << " verify_block_size: " << verify_block_size << " dev_sz: " << dev_sz;
+
+    ring_ = io_uring_cpp::IoUringInterface::CreateLinuxIoUring(queue_depth, 0);
+    if (ring_.get() == nullptr) {
+        PLOG(ERROR) << "Verify: io_uring_queue_init failed for queue_depth: " << queue_depth;
         return false;
     }
 
-    std::unique_ptr<void, decltype(&::free)> buffer(addr, ::free);
-
-    uint64_t bytes_read = 0;
-
-    while (true) {
-        size_t to_read = std::min((dev_sz - file_offset), read_sz);
-
-        if (!android::base::ReadFullyAtOffset(fd.get(), buffer.get(), to_read, file_offset)) {
-            SNAP_PLOG(ERROR) << "Failed to read block from block device: " << dm_block_device
-                             << " partition-name: " << partition_name
-                             << " at offset: " << file_offset << " read-size: " << to_read
-                             << " block-size: " << dev_sz;
+    std::unique_ptr<struct iovec[]> vecs = std::make_unique<struct iovec[]>(queue_depth);
+    std::vector<std::unique_ptr<void, decltype(&::free)>> buffers;
+    for (int i = 0; i < queue_depth; i++) {
+        void* addr;
+        ssize_t page_size = getpagesize();
+        if (posix_memalign(&addr, page_size, verify_block_size) < 0) {
+            LOG(ERROR) << "posix_memalign failed";
             return false;
         }
 
-        bytes_read += to_read;
-        file_offset += (skip_blocks * verify_block_size);
-        if (file_offset >= dev_sz) {
-            break;
+        buffers.emplace_back(addr, ::free);
+        vecs[i].iov_base = addr;
+        vecs[i].iov_len = verify_block_size;
+    }
+
+    auto ret = ring_->RegisterBuffers(vecs.get(), queue_depth);
+    if (!ret.IsOk()) {
+        SNAP_LOG(ERROR) << "io_uring_register_buffers failed: " << ret.ErrCode();
+        return false;
+    }
+
+    loff_t file_offset = 0;
+    const uint64_t read_sz = verify_block_size;
+    uint64_t total_read = 0;
+    int num_submitted = 0;
+
+    while (total_read < dev_sz) {
+        for (size_t i = 0; i < queue_depth; i++) {
+            uint64_t to_read = std::min((dev_sz - file_offset), read_sz);
+            if (to_read <= 0) break;
+
+            const auto sqe =
+                    ring_->PrepReadFixed(fd.get(), vecs[i].iov_base, to_read, file_offset, i);
+            if (!sqe.IsOk()) {
+                SNAP_PLOG(ERROR) << "PrepReadFixed failed";
+                return false;
+            }
+            file_offset += to_read;
+            total_read += to_read;
+            num_submitted += 1;
+        }
+
+        const auto io_submit = ring_->SubmitAndWait(num_submitted);
+        if (!io_submit.IsOk()) {
+            SNAP_LOG(ERROR) << "SubmitAndWait failed: " << io_submit.ErrMsg()
+                            << " for: " << num_submitted << " entries.";
+            return false;
+        }
+
+        SNAP_LOG(DEBUG) << "io_uring_submit: " << total_read << "num_submitted: " << num_submitted
+                        << "ret: " << ret;
+
+        const auto cqes = ring_->PopCQE(num_submitted);
+        if (cqes.IsErr()) {
+            SNAP_LOG(ERROR) << "PopCqe failed for: " << num_submitted
+                            << " error: " << cqes.GetError().ErrMsg();
+            return false;
+        }
+        for (const auto& cqe : cqes.GetResult()) {
+            if (cqe.res < 0) {
+                SNAP_LOG(ERROR) << "I/O failed: cqe->res: " << cqe.res;
+                return false;
+            }
+            num_submitted -= 1;
         }
     }
 
-    SNAP_LOG(DEBUG) << "Verification success with bytes-read: " << bytes_read
+    SNAP_LOG(DEBUG) << "Verification success with io_uring: "
                     << " dev_sz: " << dev_sz << " partition_name: " << partition_name;
 
     return true;
@@ -175,47 +230,13 @@ bool UpdateVerify::VerifyPartition(const std::string& partition_name,
         return false;
     }
 
-    /*
-     * Not all partitions are of same size. Some partitions are as small as
-     * 100Mb. We can just finish them in a single thread. For bigger partitions
-     * such as product, 4 threads are sufficient enough.
-     *
-     * TODO: With io_uring SQ_POLL support, we can completely cut this
-     * down to just single thread for all partitions and potentially verify all
-     * the partitions with zero syscalls. Additionally, since block layer
-     * supports polling, IO_POLL could be used which will further cut down
-     * latency.
-     */
-    int num_threads = kMinThreadsToVerify;
-    auto verify_threshold_size = android::base::GetUintProperty<uint>(
-            "ro.virtual_ab.verify_threshold_size", kThresholdSize);
-    if (dev_sz > verify_threshold_size) {
-        num_threads = kMaxThreadsToVerify;
+    if (!KernelSupportsIoUring()) {
+        SNAP_LOG(INFO) << "Kernel does not support io_uring. Skipping verification.\n";
+        // This will fallback to update_verifier to do the verification.
+        return false;
     }
 
-    std::vector<std::future<bool>> threads;
-    off_t start_offset = 0;
-    const int skip_blocks = num_threads;
-
-    auto verify_block_size =
-            android::base::GetUintProperty("ro.virtual_ab.verify_block_size", kBlockSizeVerify);
-    while (num_threads) {
-        threads.emplace_back(std::async(std::launch::async, &UpdateVerify::VerifyBlocks, this,
-                                        partition_name, dm_block_device, start_offset, skip_blocks,
-                                        dev_sz));
-        start_offset += verify_block_size;
-        num_threads -= 1;
-        if (start_offset >= dev_sz) {
-            break;
-        }
-    }
-
-    bool ret = true;
-    for (auto& t : threads) {
-        ret = t.get() && ret;
-    }
-
-    if (ret) {
+    if (VerifyBlocks(partition_name, dm_block_device, dev_sz)) {
         succeeded = true;
         UpdatePartitionVerificationState(UpdateVerifyState::VERIFY_SUCCESS);
         SNAP_LOG(INFO) << "Partition: " << partition_name << " Block-device: " << dm_block_device
