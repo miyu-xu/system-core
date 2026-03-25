5e70f97ce57dd50bd7356a4474afad374b16931e
Jindong Yue <jindong.yue@nxp.com>
2026-03-25 16:49:59 +0800

    fs_mgr: Fix max_comp_streams handling
    The max_comp_streams is initialized to zero when not explicitly set in
    fstab. This causes fs_mgr_swapon_all() to require the ZRAM_CONF_MCS
    file to exist, leading to unnecessary failures when the file is missing.
    
    In kernel v6.18+, the max_comp_streams attribute has been removed by:
      4127e13c9302 ("zram: remove max_comp_streams device attr")
    
    This causes swapon_all to always fail on newer kernels.
    
    Fix by only attempting to configure max_comp_streams when it's
    explicitly set (> 0), allowing swapon to succeed when the attribute
    is not configured or not supported by the kernel.
    
    Change-Id: Ic1579fc94146e25294c5c7c6f60b7875fd54a298
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>

--- fs_mgr/fs_mgr.cpp
+++ fs_mgr/fs_mgr.cpp
@@ -2191,7 +2191,7 @@ bool fs_mgr_swapon_all(const Fstab& fstab) {
             // device.  There is no point in having multiple zram devices
             // on a system (all the memory comes from the same pool) so
             // we can assume the device number is 0.
-            if (entry.max_comp_streams >= 0) {
+            if (entry.max_comp_streams > 0) {
                 auto zram_mcs_fp = std::unique_ptr<FILE, decltype(&fclose)>{
                         fopen(ZRAM_CONF_MCS, "re"), fclose};
                 if (zram_mcs_fp == nullptr) {
